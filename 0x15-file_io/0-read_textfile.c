44346130ddbd99ec53f79433ef1e4bab826f232a
esanpelumi <esannpelumii@gmail.com>
2023-04-10 17:03:56 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,29 @@
-/**
+#include "main.h"
+#include <stdlib.h>
 
-*read_textfile - Reads a text file and prints to STDOUT.
-*@filename: The name of the file to be read.
-*@letters: The number of letters to be read.
-*return: The actual number of bytes read and printed.
-*vbnet
-*Copy code
-* 0 when function fails or filename is NULL.
+/**
+* read_file - Reads a file and prints its content to STDOUT
+* @filename: Name of the file to be read
+* @max_bytes: Maximum number of bytes to be read
+* Return: Number of bytes read and printed, 0 on failure or if filename is NULL
 */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_file(const char *filename, size_t max_bytes)
 {
 	char *buffer;
 	ssize_t file_descriptor;
 	ssize_t bytes_read;
 	ssize_t bytes_written;
-	
+
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	bytes_read = read(file_descriptor, buffer, letters);
+	buffer = malloc(sizeof(char) * max_bytes);
+	bytes_read = read(file_descriptor, buffer, max_bytes);
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	free(buffer);
 	close(file_descriptor);
+
 	return (bytes_written);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,13 @@
-#ifndef MY_UTILS_H
-#define MY_UTILS_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-ssize_t read_file(const char *filepath, size_t bufsize);
-int create_file(const char *filepath, char *filecontent);
-int append_text(const char *filepath, char *filecontent);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
